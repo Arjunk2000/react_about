80918a455c7082a416c453ccc754125ab88c8e16
Arjunk2000 <96836318+Arjunk2000@users.noreply.github.com>
2022-12-24 19:12:10 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+
+function fun() {
+  return (
+    <div className="App">
+      <h1>About Us</h1>
+      <p className="p1">
+        At Zappos.com, our purpose is simple: to live and deliver WOW
+      </p>
+      <p className="p2">
+        Twenty years ago, we began as a small online retailer that only sold
+        shoes. Today, we still sell shoes as well as clothing, handbags,
+        accessories, and more. That "more" is providing the very best customer
+        service, customer experience, and company culture. We aim to inspire the
+        world by showing it's possible to simultaneously deliver happiness to
+        customers, employees, vendors, shareholders, and the community in a
+        long-term sustainable way.
+      </p>
+      <p className="p3">
+        We hope that in the future people won't even realize we started selling
+        shoes online. Instead they will know that Zappos as a service company
+        that just happens to sell _______
+      </p>
+    </div>
+  );
+}
+
+export default fun;
